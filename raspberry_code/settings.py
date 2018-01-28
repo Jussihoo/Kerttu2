df4a69cd7c6446856a64dc9dfd71675000c5f59c
Jussihoo <juhani.hakosalo@gmail.com>
2018-01-28 21:58:18 +0200

    Update settings.py

--- raspberry_code/settings.py
+++ raspberry_code/settings.py
@@ -4,3 +4,4 @@ KERTTU_SERVER_URL = 'kerttu_server_url_here'
 LOC_ID_1 = 1 # ruuvitag outside
 LOC_ID_2 = 2 # ruuvitag inside
 MEASUREMENT_INTERVAL = 10 * 60.0 # 10 minutes
+SUPERVISION_PERIOD = 30.0 # 30 seconds
