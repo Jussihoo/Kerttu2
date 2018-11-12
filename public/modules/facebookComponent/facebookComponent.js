40aa71825c089174bf911e8453079f7b4fc66cac
Jussihoo <juhani.hakosalo@gmail.com>
2018-11-12 21:00:08 +0200

    improvements to the Facebook page plugin functionality

--- public/modules/facebookComponent/facebookComponent.js
+++ public/modules/facebookComponent/facebookComponent.js
@@ -17,6 +17,24 @@
             var vm = this;
             vm.fbPageUrl = constants.FB_PAGE_URL;
             vm.fbAppId = constants.FB_APP_ID;
+            
+            vm.loadFBPlugin = function(d, s, id) {
+                var js, fjs = d.getElementsByTagName(s)[0];
+                if (window.FB) {
+                  return;
+                }
+                js = d.createElement(s); js.id = id;
+                js.src = 'https://connect.facebook.net/fi_FI/sdk.js#xfbml=1&version=v3.2&appId={{ vm.fbAppId }}';
+                fjs.parentNode.insertBefore(js, fjs);
+            };
+            
+            vm.loadFBPlugin(document, 'script', 'facebook-jssdk');
+            
+            angular.element(document).ready(function () {
+                if (window.FB) {
+                    window.FB.XFBML.parse();
+                }
+            });
         }
         
 }());
